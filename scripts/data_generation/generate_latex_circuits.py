028f6884a0a1164f53a342be32aa064627010937
Timothé Chaumont <timchaumont@gmail.com>
2022-01-04 19:11:12 +0100

    start track file

---
+++ scripts/data_generation/generate_latex_circuits.py
@@ -0,0 +1,11 @@
+
+# tokens have to be divided into type classes
+def generate_token_sequence():
+    """ given a seed(?), generate a token's list corresponding to a belivable circuit"""
+    pass
+
+def convert_tokens_to_latex():
+    pass
+
+def save_to_latex():
+    pass
