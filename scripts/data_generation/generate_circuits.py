6862dfa39267ee3f03ed9792aa8896f58853f6ce
Timothé Chaumont <timchaumont@gmail.com>
2022-02-11 19:40:55 +0100

    refactor basic circuit generator

--- scripts/data_generation/generate_circuits.py
+++ scripts/data_generation/generate_circuits.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# later : use objects instead of dictionnaries
 
 BIPOLE_TOKENS = ["short", "open", "generic",
                  "capacitor", "cute inductor",
@@ -8,48 +9,43 @@ BIPOLE_TOKENS = ["short", "open", "generic",
                  "normal open switch"]
 
 
-def generate_one_loop_circuit():
-    """Generates an eletrical circuit in the form of a tokens list 
-       These circuits are composed of a single loop
+def generate_one_loop_circuit(bipole_tokens=BIPOLE_TOKENS):
+    """Generates an eletrical circuit in the form of a list of bipole elements
+       The generated circuits are composed of one single loop
 
        generate a random size for corresponding segments, and then generate a random number of elements
+
+       Returns:
+          A list of dictionnaries with keys : 
+          - "from" : the position of the before node
+          - "to" : the position of the after node
+          - "type" : the kind of electrical bipole 
     """
     segments_list = []
-    nb_segments = 4
+    nb_vert_segments = 2
+    nb_horiz_segments = 2
     # generate the 'shape' of the circuit
     vert_max_nb_elts, horiz_max_nb_elts = np.random.randint(1, 4, size=2)
 
-    # bottom segment
-    nb_elements = np.random.randint(1, horiz_max_nb_elts+1)
-    for e in range(nb_elements):
-        bipole = np.random.choice(BIPOLE_TOKENS)
-        element = {"from": (2*e, 0), "to": (2*e + 2, 0), "type": bipole}
-        segments_list.append(element)
+    # bottom & top segments
+    for s in range(nb_horiz_segments):
+        y_pos = 2*s
+        nb_elements = np.random.randint(1, horiz_max_nb_elts+1)
+        for e in range(nb_elements):
+            bipole = np.random.choice(bipole_tokens)
+            element = {"from": (2*e, y_pos), "to": (2*e +
+                                                    2, y_pos), "type": bipole}
+            segments_list.append(element)
 
-    # left segment
-    nb_elements = np.random.randint(1, vert_max_nb_elts+1)
-    for e in range(nb_elements):
-        bipole = np.random.choice(BIPOLE_TOKENS)
-        element = {"from": (0, 2*e), "to": (0, 2*e + 2), "type": bipole}
-        segments_list.append(element)
-
-    # top segment
-    nb_elements = np.random.randint(1, horiz_max_nb_elts+1)
-    y_pos = vert_max_nb_elts * 2
-    for e in range(nb_elements):
-        bipole = np.random.choice(BIPOLE_TOKENS)
-        element = {"from": (2*e, y_pos), "to": (2*e + 2,
-                                                y_pos), "type": bipole}
-        segments_list.append(element)
-
-    # right segment
-    nb_elements = np.random.randint(1, vert_max_nb_elts+1)
-    x_pos = horiz_max_nb_elts * 2
-    for e in range(nb_elements):
-        bipole = np.random.choice(BIPOLE_TOKENS)
-        element = {"from": (x_pos, 2*e), "to": (x_pos,
-                                                2*e + 2), "type": bipole}
-        segments_list.append(element)
+    # left & right segment
+    for s in range(nb_vert_segments):
+        x_pos = 2*s
+        nb_elements = np.random.randint(1, vert_max_nb_elts+1)
+        for e in range(nb_elements):
+            bipole = np.random.choice(bipole_tokens)
+            element = {"from": (x_pos, 2*e), "to": (x_pos,
+                                                    2*e + 2), "type": bipole}
+            segments_list.append(element)
 
     return segments_list
 
