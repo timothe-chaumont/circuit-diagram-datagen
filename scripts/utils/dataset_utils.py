50a4202b9392206972a3d09c2b872c43045973aa
Timothé Chaumont <timchaumont@gmail.com>
2022-04-09 14:46:29 +0200

    add image & text transformations

--- scripts/utils/dataset_utils.py
+++ scripts/utils/dataset_utils.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 from torchvision.io import read_image
+import torchvision.transforms as T
+import torch.nn.functional as F
 from torch.utils.data import Dataset
 
 from scripts.preprocessing.preprocess_formulas import Vocabulary
@@ -10,10 +12,19 @@ class CustomCircuitDataset(Dataset):
     LINE_INDEX = 0
     IMG_NAMES_INDEX = 1
 
-    def __init__(self, annotations_file: str, formulas_file: str, img_dir: str, transform=None, target_transform=None):
+    def __init__(
+        self,
+        annotations_file: str,
+        formulas_file: str,
+        img_dir: str,
+        transform=T.Lambda(lambda t: t / 255),  # normalize the image to [0, 1]
+        target_transform=F.one_hot,  # convert the label to one-hot vector
+    ):
         # read formula line, image name and version
-        self.circuit_data = pd.read_csv(annotations_file, sep=' ', header=None)
-        self.formulas = pd.read_csv(formulas_file, sep='#', header=None) # delimiter character # is not in the dataset
+        self.circuit_data = pd.read_csv(annotations_file, sep=" ", header=None)
+        self.formulas = pd.read_csv(
+            formulas_file, sep="#", header=None
+        )  # delimiter character # is not in the dataset
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
@@ -27,18 +38,22 @@ class CustomCircuitDataset(Dataset):
 
     def __getitem__(self, idx):
         """Args:
-            idx (int): Index of the example between 0 and nb_exambles - 1.
+        idx (int): Index of the example between 0 and nb_exambles - 1.
         """
-        img_path = os.path.join(self.img_dir, 
-            self.circuit_data.iloc[idx, self.IMG_NAMES_INDEX]) + ".jpg"
-        image = read_image(img_path)
+        img_path = (
+            os.path.join(
+                self.img_dir, self.circuit_data.iloc[idx, self.IMG_NAMES_INDEX]
+            )
+            + ".jpg"
+        )
+        image = read_image(img_path)  # images are already in grey scale (1 channel)
         formula_line = self.circuit_data.iloc[idx, self.LINE_INDEX]
         # read circuit formula
-        formula_str = self.formulas.iloc[formula_line-1, 0]
+        formula_str = self.formulas.iloc[formula_line - 1, 0]
         formula = self.vocab.preprocess_formula(formula_str)
         # apply possible transformations
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
-            label = self.target_transform(label)
+            formula = self.target_transform(formula, len(self.vocab))
         return image, formula
